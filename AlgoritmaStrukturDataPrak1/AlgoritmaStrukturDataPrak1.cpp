21aa1afeb17c66d14eee848d850bf271a61ef5ee
tacosz <nabilnasruddin.offc@gmail.com>
2024-03-07 19:48:12 +0700

    penambahan cout

--- AlgoritmaStrukturDataPrak1/AlgoritmaStrukturDataPrak1.cpp
+++ AlgoritmaStrukturDataPrak1/AlgoritmaStrukturDataPrak1.cpp
@@ -49,6 +49,7 @@ void display() {
     cout << "================================" << endl;
     cout << "Elemen Array yang telah tersusun" << endl;
     cout << "================================" << endl;
+    cout << endl;
     for (int j = 0; j < n; j++) {
         cout << arr[j] << endl; //output setiap element array pada garis baru
         if (j < n - 1) {
