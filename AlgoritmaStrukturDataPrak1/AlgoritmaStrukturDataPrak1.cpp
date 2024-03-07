d8ac8428df163ac1dd9e72d055eb5d3991c80ea8
tacosz <nabilnasruddin.offc@gmail.com>
2024-03-07 19:03:00 +0700

    perubahan nama array dari a menjadi arr

--- AlgoritmaStrukturDataPrak1/AlgoritmaStrukturDataPrak1.cpp
+++ AlgoritmaStrukturDataPrak1/AlgoritmaStrukturDataPrak1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int a[20];
+int arr[20];
 int n;
 
+void input() { //procedure
+
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "Array dapat mempunyai maksimal 20 elemen" << endl;
+        }
+    }
+    cout << "Masukkan elemen array" << endl;
+
+    for (int i = 0; i < n; i++)
+        cout << "Data ke-" << (i + 1) << ":";
+    cin >> arr[i];
+}
 
 }
 int main() {
